41710c8c284546f27885dc6d880d7bfe99e22caf
SCN\christopher.lang <christopher.lang@stockport.gov.uk>
2020-02-26 09:31:18 +0000

    first commit

---
+++ src/Configuration.js
@@ -0,0 +1,74 @@
+import { prowPopup } from './Popups'
+import { prowStyle } from './Styles'
+
+const Configuration = {
+    Map: {
+        StartingLatLng: [53.3915, -2.125143],
+        StartingZoom: 12,
+        FullscreenControl: true,
+        DisplayLayerControls: true,
+        DisplayGrayScale: true,
+        DisplayStreets: true,
+        EnableAddressSearch: true
+    },
+    DynamicData: 
+    [
+        {
+            key: 'Public Rights of Way',
+            url: 'http://scnmapsrv.stockport.gov.uk:8080/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=highways:public_rights_of_way&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            layerOptions: {
+                onEachFeature: prowPopup,
+                maxZoom: 2,
+                style: prowStyle
+            },
+            displayOverlay: true,
+            visibleByDefault: true 
+        },
+        {
+            key: 'os1250_line',
+            url: 'http://scnmapsrv.stockport.gov.uk:8080/geoserver/wms?',
+            layerOptions: {
+                maxZoom: 20,
+                minZoom: 19,
+                layers: 'base_maps:os1250_line',
+                format: 'image/png',
+                transparent: true
+            },
+            displayOverlay: false,
+            visibleByDefault: true 
+        },
+        {
+            key: 'os1250_text',
+            url: 'http://scnmapsrv.stockport.gov.uk:8080/geoserver/wms?',
+            layerOptions: {
+                maxZoom: 20,
+                minZoom: 19,
+                layers: 'base_maps:os1250_text',
+                format: 'image/png',
+                transparent: true
+            },
+            displayOverlay: false,
+            visibleByDefault: true 
+        }
+    ],
+    StaticData: 
+    [
+        {
+            key: 'boundary',
+            url: 'https://spatialgeojson.s3-eu-west-1.amazonaws.com/webmapping/boundary.geojson',
+            layerOptions: {
+                interactive: false,
+                maxZoom: 9,
+                style: {
+                    color: '#000',
+                    weight: 4,
+                    opacity: 1,
+                    fillColor: '#000',
+                    fillOpacity: 0
+                }
+            }
+        }
+    ]
+}
+
+export default Configuration
---
+++ src/Styles/index.js
@@ -0,0 +1,13 @@
+const prowStyle = {
+    color: '#006d2c',
+    weight: 4,
+    opacity: 1,
+    fillColor: '#fec44f',
+    fillOpacity: 0,
+    dashArray: '7,7,7',
+    dashOffset: '0'
+}
+
+export {
+prowStyle
+}
