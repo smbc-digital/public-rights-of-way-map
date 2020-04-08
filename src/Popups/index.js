6c46bfd41839027e5f23f11098e4ac32e206867d
Colin Lees <colinleesa7x@gmail.com>
2020-04-08 16:36:44 +0100

    fix: font awesome icons and page title

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,6 +1,6 @@
 const prowPopup = (feature, layer) => {
   
-  const content = `<div class="item"><i class="fas fa-map-signs" aria-hidden="true"></i><p class="title">Public Right of Way</p>
+  const content = `<div class="item"><i class="fa fa-map-signs" aria-hidden="true"></i><p class="title">Public Right of Way</p>
   <p></p>
   <p class="info">PROW Number: ${feature.properties.row}</p>
   <p class="info">Type: ${feature.properties.type}</p>
