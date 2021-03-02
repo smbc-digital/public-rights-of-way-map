a8f2cd072d346daec84136adfd01e47764f12223
Lydia Gray <lydia.gray@stockport.gov.uk>
2021-03-02 15:02:08 +0000

    fix(project): include DS stylesheet in solution

--- src/App.js
+++ src/App.js
@@ -155,7 +155,7 @@ function App() {
   }
 
   return (
-    <div id="map" />
+    <div id="map" className={Config.Map.Class} />
   )
 }
 
--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,10 +1,14 @@
 const prowPopup = (feature, layer) => {
   
-  const content = `<div class="item"><i class="fa fa-map-signs" aria-hidden="true"></i><p class="title">Public Right of Way</p>
-  <p></p>
-  <p class="info">PROW Number: ${feature.properties.row}</p>
-  <p class="info">Type: ${feature.properties.type}</p>
-  
+  const content = `<div class="smbc-map__item">
+    <div class="smbc-map__item__header__block">
+      <i class="fas fa-map-signs smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <span class="smbc-map__item__header__block__title">Public Right of Way</span>
+    </div>
+    <div class="smbc-map__item__body">
+      <p>PROW Number: ${feature.properties.row}</p>
+      <p>Type: ${feature.properties.type}</p>
+    </div>
   </div>`
  
   layer.bindPopup(content)
